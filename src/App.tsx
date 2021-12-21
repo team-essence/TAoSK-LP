497b971d0f686151ead206371861807e87b0975f
roll1226 <tomo_yasu_4724@icloud.com>
2021-12-22 00:49:05 +0900

    feat: provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from "react";
+
 import { GlobalStyle } from "styles/global/globalStyle";
+import styled from "styled-components";
+import { AppProvider } from "provider/AppProvider";
 
 const App: FC = () => {
   return (
-    <div>
-      <GlobalStyle />
-    </div>
+    <AppProvider>
+      <AppContainer>
+        <GlobalStyle />
+      </AppContainer>
+    </AppProvider>
   );
 };
+
+const AppContainer = styled.div`
+  height: 200vh;
+`;
 
 export default App;
---
+++ src/provider/AppProvider.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "styles/global/theme";
+
+type Props = { children: ReactNode };
+
+export const AppProvider: FC<Props> = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
