a893de9115064e6491b6a46f8fa24a2d0797d79c
h-jun01 <jyun.0110@icloud.com>
2021-12-23 14:34:38 +0900

    add mediaQuery

---
+++ src/styles/mediaQuery/mediaQuery.ts
@@ -0,0 +1,25 @@
+type Media = "xs" | "sm" | "md" | "lg" | "xl";
+
+type Breakpoints = {
+  [T in Media]: number;
+};
+
+const breakpoints: Breakpoints = {
+  xs: 400,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1024,
+};
+
+/**
+ * @example
+ * ${mediaQueries("md")`
+ *  background-color: green;
+ *`};
+ **/
+
+export const mediaQuery = (key: keyof typeof breakpoints) => {
+  return (style: TemplateStringsArray | String) =>
+    `@media (min-width: ${breakpoints[key]}px) { ${style} }`;
+};
