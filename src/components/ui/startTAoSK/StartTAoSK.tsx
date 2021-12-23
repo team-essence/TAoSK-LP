d5422d7535eafb6d3e2cd9c58406763fe47727ab
h-jun01 <jyun.0110@icloud.com>
2021-12-24 01:58:59 +0900

    add: Responsive support

--- src/components/ui/startTAoSK/StartTAoSK.tsx
+++ src/components/ui/startTAoSK/StartTAoSK.tsx
@@ -1,14 +1,24 @@
 import React, { FCX } from "react";
 import { calculateVwBasedOnFigma } from "utils/figma/calculateSizeBasedOnFigma";
 import { mediaQuery } from "utils/response/mediaQuery";
+import { useWatchInnerAspect } from "hooks/useWatchInnerAspect";
 import styled, { css } from "styled-components";
 
 export const StartTAoSK: FCX = ({ className }) => {
+  const { innerWidth } = useWatchInnerAspect();
+
   return (
     <StyledContainer className={className}>
       <StyledInnerBorder>
         <h3>
-          <StyledTitle src="/startTaosk/title.svg" alt="title" />
+          <StyledTitle
+            src={
+              innerWidth >= 576
+                ? "/startTaosk/title.svg"
+                : "/startTaosk/sm-title.svg"
+            }
+            alt="title"
+          />
         </h3>
         <StyledFlexContainer>
           <div>
@@ -21,7 +31,11 @@ export const StartTAoSK: FCX = ({ className }) => {
             <StyledStartButton>
               <a href="!#">
                 <StyledStartButtonImage
-                  src="/startTaosk/start.svg"
+                  src={
+                    innerWidth >= 576
+                      ? "/startTaosk/start.svg"
+                      : "/startTaosk/sm-start.svg"
+                  }
                   alt="今すぐ始める"
                 />
               </a>
@@ -39,6 +53,9 @@ const StyledContainer = styled.div`
   border-radius: 10px;
   padding: ${calculateVwBasedOnFigma(10)};
   margin: 0 auto;
+  ${mediaQuery.sm`
+     width: 371px;
+  `}
 `;
 const StyledInnerBorder = styled.div`
   ${({ theme }) => css`
@@ -47,19 +64,33 @@ const StyledInnerBorder = styled.div`
     border: 4px solid ${theme.COLORS.BORDER.WHITE};
     border-radius: 10px;
     padding: ${calculateVwBasedOnFigma(60)} 0;
+    ${mediaQuery.sm`
+      padding: 40px 0;
+    `}
   `}
 `;
 const StyledTitle = styled.img`
   width: ${calculateVwBasedOnFigma(711)};
   padding-bottom: ${calculateVwBasedOnFigma(40)};
+  ${mediaQuery.sm`
+    width: 331px;
+    padding-bottom: 40px;
+  `}
 `;
 const StyledMainVisual = styled.img`
   width: ${calculateVwBasedOnFigma(450)};
   height: ${calculateVwBasedOnFigma(282)};
   object-fit: cover;
+  ${mediaQuery.sm`
+    width: 314px;
+    height: 196px;
+  `}
 `;
 const StyledStartButtonImage = styled.img`
   width: ${calculateVwBasedOnFigma(314)};
+  ${mediaQuery.sm`
+    width: 314px;
+  `}
 `;
 const StyledStartButton = styled.button`
   display: block;
@@ -74,6 +105,7 @@ const StyledText = styled.p`
   `}
   ${mediaQuery.sm`
     -webkit-text-stroke: 2px transparent;
+    font-size: 16px;
   `}
 `;
 const StyledFlexContainer = styled.div`
@@ -89,8 +121,14 @@ const StyledRightWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   width: ${calculateVwBasedOnFigma(365)};
+  ${mediaQuery.sm`
+    width: 314px;
+  `}
   p {
     text-align: center;
     padding-bottom: ${calculateVwBasedOnFigma(40)};
+    ${mediaQuery.sm`
+      padding: 40px 0;
+    `}
   }
 `;
