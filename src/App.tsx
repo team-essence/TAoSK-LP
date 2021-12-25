be33669fbe25a173daed359ae4dccfd62c671a3e
h-jun01 <jyun.0110@icloud.com>
2021-12-25 17:28:22 +0900

    fix: view

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,12 @@ import React, { FC } from 'react'
 import { GlobalStyle } from 'styles/global/globalStyle'
 import { AppProvider } from 'provider/AppProvider'
 import { View } from 'View'
-import { leftScrollImage, rightScrollImage } from 'consts/carouselImage'
-import { Carousel } from 'components/ui/carousel/Carousel'
-import { StartTAoSK } from 'components/ui/startTAoSK/StartTAoSK'
-import { SiteFooter } from 'components/ui/footer/SiteFooter'
 
 const App: FC = () => {
   return (
     <AppProvider>
       <GlobalStyle />
       <View />
-      <Carousel direction="left" images={leftScrollImage} />
-      <Carousel direction="right" images={rightScrollImage} />
-      <StartTAoSK />
-      <SiteFooter />
     </AppProvider>
   )
 }
