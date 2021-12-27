28b88f2d0366dfa243c10c6dfaa8aae1833497e9
h-jun01 <jyun.0110@icloud.com>
2021-12-28 02:33:18 +0900

    add: video area

--- src/View.tsx
+++ src/View.tsx
@@ -1,6 +1,7 @@
 import React, { FCX } from 'react'
-import { FirstViewHeader } from 'components/ui/header/FirstViewHeader'
 import { leftScrollImage, rightScrollImage } from 'consts/carouselImage'
+import { FirstViewHeader } from 'components/ui/header/FirstViewHeader'
+import { VideoArea } from 'components/ui/videoArea/VideoArea'
 import { Carousel } from 'components/ui/carousel/Carousel'
 import { StartTAoSK } from 'components/ui/startTAoSK/StartTAoSK'
 import { SiteFooter } from 'components/ui/footer/SiteFooter'
@@ -10,6 +11,7 @@ export const View: FCX = ({ className }) => {
   return (
     <ViewContainer className={className}>
       <FirstViewHeader />
+      <VideoArea />
       <Carousel direction="left" images={leftScrollImage} />
       <Carousel direction="right" images={rightScrollImage} />
       <StartTAoSK />
