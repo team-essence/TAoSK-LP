c65d05992b3d188743aaa0d2eba646191f807eae
h-jun01 <jyun.0110@icloud.com>
2021-12-28 17:41:44 +0900

    add: HP,MP modal

--- src/components/ui/modal/HPandMPVisualizationModal.tsx
+++ src/components/ui/modal/HPandMPVisualizationModal.tsx
@@ -14,7 +14,37 @@ export const HPandMPVisualizationModal: FCX<Props> = ({ className }) => {
 
   return (
     <StyledContainer className={className} ref={ref}>
-      {inView && <Modal title="HP,MPの見える化">aaaaaaaaaaaaaaaaaaaa</Modal>}
+      {inView && (
+        <Modal title="HP,MPの見える化">
+          <StyledWrap>
+            <div>
+              <StyledTitleImage
+                src="/modal/hp_mp_title.png"
+                alt="HP,MPを脳波でリアルタイム解析"
+                loading="lazy"
+              />
+            </div>
+            <StyledFlexContainer>
+              <div>
+                <StyledKageyamaImage src="/modal/kageyama.png" alt="kageyama" loading="lazy" />
+              </div>
+              <div>
+                <StyledMindWaveImage src="/modal/mind_wave.png" alt="mind_wave" loading="lazy" />
+              </div>
+              <StyledKurauchiContainer>
+                <StyledKurauchiImage src="/modal/kurauchi.png" alt="kurauchi" loading="lazy" />
+              </StyledKurauchiContainer>
+            </StyledFlexContainer>
+            <StyledTextImageContainer>
+              <StyledTextImage
+                src="/modal/hp_mp_text.png"
+                alt="顔認証で仲間の名前,役職,HP,MPを表示"
+                loading="lazy"
+              />
+            </StyledTextImageContainer>
+          </StyledWrap>
+        </Modal>
+      )}
     </StyledContainer>
   )
 }
@@ -23,4 +53,35 @@ const StyledContainer = styled.div`
   width: ${calculateMinSizeBasedOnFigma(1120)};
   height: ${calculateMinSizeBasedOnFigma(573)};
   margin: 0 auto;
+`
+const StyledWrap = styled.div`
+  position: relative;
+`
+const StyledFlexContainer = styled.div`
+  display: flex;
+  gap: ${calculateMinSizeBasedOnFigma(16)};
+`
+const StyledTitleImage = styled.img`
+  width: ${calculateMinSizeBasedOnFigma(607)};
+  padding-bottom: ${calculateMinSizeBasedOnFigma(16)};
+`
+const StyledKageyamaImage = styled.img`
+  width: ${calculateMinSizeBasedOnFigma(394)};
+`
+const StyledMindWaveImage = styled.img`
+  width: ${calculateMinSizeBasedOnFigma(220)};
+`
+const StyledKurauchiContainer = styled.div`
+  transform: translateY(-41%);
+`
+const StyledKurauchiImage = styled.img`
+  width: ${calculateMinSizeBasedOnFigma(316)};
+`
+const StyledTextImageContainer = styled.div`
+  position: absolute;
+  bottom: ${calculateMinSizeBasedOnFigma(-12)};
+  right: 0;
+`
+const StyledTextImage = styled.img`
+  width: ${calculateMinSizeBasedOnFigma(569)};
 `
