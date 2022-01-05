d0daa1a4c3ce39137ead9c9bb4548cb5f72c4852
h-jun01 <jyun.0110@icloud.com>
2022-01-06 00:28:26 +0900

    fix: Fixed phone size for loading.

--- src/components/ui/loading/Loading.tsx
+++ src/components/ui/loading/Loading.tsx
@@ -1,5 +1,6 @@
 import React, { FCX, useEffect } from 'react'
 import { calculateMinSizeBasedOnFigmaWidth } from 'utils/figma/calculateSizeBasedOnFigma'
+import { mediaQuery } from 'utils/response/mediaQuery'
 import nowLoading from './loading.json'
 import lottie from 'lottie-web'
 import styled from 'styled-components'
@@ -29,5 +30,8 @@ const StyledContainer = styled.div`
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND.MINE_SHAFT};
 `
 const StyledNowLoading = styled.div`
+  ${mediaQuery.sm`
+    width: ${calculateMinSizeBasedOnFigmaWidth(1100)};
+  `}
   width: ${calculateMinSizeBasedOnFigmaWidth(800)};
 `
