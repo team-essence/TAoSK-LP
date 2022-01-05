511eacefc45f206b94d28cf48a184f44cdc047b0
h-jun01 <jyun.0110@icloud.com>
2022-01-06 00:11:17 +0900

    add: loading animation

---
+++ src/components/ui/loading/Loading.tsx
@@ -0,0 +1,33 @@
+import React, { FCX, useEffect } from 'react'
+import { calculateMinSizeBasedOnFigmaWidth } from 'utils/figma/calculateSizeBasedOnFigma'
+import nowLoading from './loading.json'
+import lottie from 'lottie-web'
+import styled from 'styled-components'
+
+export const Loading: FCX = () => {
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector('#now-loading') as HTMLDivElement,
+      renderer: 'svg',
+      animationData: JSON.parse(JSON.stringify(nowLoading) || 'null'),
+    })
+
+    return () => lottie.stop()
+  }, [])
+
+  return (
+    <StyledContainer>
+      <StyledNowLoading id="now-loading" />
+    </StyledContainer>
+  )
+}
+
+const StyledContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND.MINE_SHAFT};
+`
+const StyledNowLoading = styled.div`
+  width: ${calculateMinSizeBasedOnFigmaWidth(800)};
+`
